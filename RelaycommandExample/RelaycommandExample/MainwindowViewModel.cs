8e565ff8fff191196c8da595bce6e7d3899daed1
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2020-04-06 14:41:28 +0530

    Add files via upload

---
+++ RelaycommandExample/RelaycommandExample/MainwindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RelaycommandExample
+{
+    class MainwindowViewModel
+    {
+        private ICommand _command;
+
+        public ICommand Command
+        {
+            get
+            {
+                return _command ?? (_command = new RelayCommand(
+                   x =>
+                   {
+                       DoStuff();
+                   }));
+            }
+
+        }
+
+        private static void DoStuff()
+        {
+            MessageBox.Show("Responding to button click event...");
+        }
+    }
+}
